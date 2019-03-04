dbdb769a6b637414326a7b9ccba34c33eef0a9c1
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2019-03-03 20:49:05 -0500

    Organized heap functions into class

--- fourGreatestIntegers.js
+++ fourGreatestIntegers.js
@@ -4,83 +4,85 @@ in O(n) time.
 */
 
 /*
-I'm assuming that the list is in the form of an array.
-I'll treat the array as if it is a binary tree and then switch the elements to make it a max heap
-I will then traverse the root, its children and one of its grandchildren (here I will need to
-compare the two)
+First, build a max heap, which takes O(n) time. Then remove four elements,
+reheapifying each time, which takes O(k * log(k)).  However, since this
+k is constant in this problem, the removal process takes constant time,
+making the entire operation of O(n) time complexity.
 */
 
-// class Heap {
-//   constructor(list) {
-
-//   }
-// }
-const main = list => {
-  if (list.length < 4) {
-    return list
+class Heap {
+  constructor(list) {
+    this.list = list
+    this.nodeOutOfPlace = false
   }
 
-  let nodeOutOfPlace = false
-
-  const heapify = i => {
+  heapify(i) {
     // each node's children are at 2i + 1 and 2i + 2
     let child1Index = 2 * i + 1
     let child2Index = 2 * i + 2
 
     // identify children and handle null nodes
-    let child1 = list[child1Index] !== undefined ? list[child1Index] : -Infinity
-    let child2 = list[child2Index] !== undefined ? list[child2Index] : -Infinity
+    let child1 =
+      this.list[child1Index] !== undefined ? this.list[child1Index] : -Infinity
+    let child2 =
+      this.list[child2Index] !== undefined ? this.list[child2Index] : -Infinity
 
     // check if parent is less than or equal to children
-    if (child1 >= list[i] || child2 >= list[i]) {
-      nodeOutOfPlace = true
+    if (child1 >= this.list[i] || child2 >= this.list[i]) {
+      this.nodeOutOfPlace = true
 
       if (child1 >= child2) {
         // swap child1 with parent
-        const temp = list[i]
-        list[i] = child1
-        list[child1Index] = temp
+        const temp = this.list[i]
+        this.list[i] = child1
+        this.list[child1Index] = temp
       } else {
         // swap child2 with parent
-        const temp = list[i]
-        list[i] = child2
-        list[child2Index] = temp
+        const temp = this.list[i]
+        this.list[i] = child2
+        this.list[child2Index] = temp
       }
     }
   }
 
-  const buildMaxHeap = () => {
+  buildMaxHeap() {
     do {
-      nodeOutOfPlace = false
-      for (let i = Math.floor(list.length / 2) - 1; i >= 0; i--) {
-        heapify(i)
+      this.nodeOutOfPlace = false
+      for (let i = Math.floor(this.list.length / 2) - 1; i >= 0; i--) {
+        this.heapify(i)
       }
-    } while (nodeOutOfPlace)
-    return list
+    } while (this.nodeOutOfPlace)
+    return this.list
   }
 
-  const remove = () => {
-    const removedElement = list[0]
-    list[0] = list[list.length - 1]
-    list.pop()
+  remove() {
+    const removedElement = this.list[0]
+    this.list[0] = this.list[this.list.length - 1]
+    this.list.pop()
 
     do {
-      nodeOutOfPlace = false
-      for (let i = 0; i < Math.floor(list.length / 2); i++) {
-        heapify(i)
+      this.nodeOutOfPlace = false
+      for (let i = 0; i < Math.floor(this.list.length / 2); i++) {
+        this.heapify(i)
       }
-    } while (nodeOutOfPlace)
+    } while (this.nodeOutOfPlace)
 
     return removedElement
   }
-
-  buildMaxHeap()
-  const fourthGreatest = []
-  for (let i = 0; i < 4; i++) {
-    fourthGreatest.push(remove())
+}
+const getFourthLargestIntegers = list => {
+  if (list.length < 4) {
+    return list
   }
 
-  return fourthGreatest
+  const heap = new Heap(list)
+  heap.buildMaxHeap()
+  const fourthLargest = []
+  for (let i = 0; i < 4; i++) {
+    fourthLargest.push(heap.remove())
+  }
+
+  return fourthLargest
 }
 
-module.exports = main
+module.exports = getFourthLargestIntegers
